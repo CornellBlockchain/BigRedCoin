3b2c1e63616a8d4b35c3cca0bfed15c327b1fa62
Ohad R Koronyo <ork6@cornell.edu>
2019-04-17 17:03:12 -0400

    changes

--- src/Fork.ml
+++ src/Fork.ml
@@ -1,26 +1,27 @@
+open BatBig_int
 type blockstore = Block.t Store.t
-type t = {head : Block.t; state : State.t; total_diff : int}
+type t = {head : Block.t; state : State.t; total_diff : big_int}
 
 let head t = t.head
 
 let state t = t.state
 
 (* [check_hash block] checks that the hash of the block is below its total difficulty. *)
-let check_hash block = failwith "unimplemented"
+let check_hash block = parent_hash block
 
 let total_diff t = t.total_diff
 
-(* [validate t block] checks that [block] is a valid block to attach to the 
+(* [validate t block] checks that [block] is a valid block to attach to the
  * head of fork [t]. *)
-let validate t block = 
-  Block.next_difficulty (head t) = Block.difficulty block 
+let validate t block =
+  Block.next_difficulty (head t) = Block.difficulty block
   && check_hash block
   && Block.height block = Block.height (head t) + 1
   && Block.parent_hash block = Block.hash (head t)
 
 (* TODO: use actual interfaces for state and block when they are written. *)
-let apply t block store = 
-  if validate t block then 
+let apply t block store =
+  if validate t block then
     Store.put store block;
   Some {head = block; state = State.update state block; total_diff = t.total_diff + Block.difficulty block}
 else None
--- src/block.ml
+++ src/block.ml
@@ -1,8 +1,9 @@
+open BatBig_int
 type header = {
-  parent_hash : string; 
+  parent_hash : string;
   beneficiary : string;
   root : string;
-  difficulty : int;
+  difficulty : big_int;
   height : int;
   timestamp : int;
   nonce : int;
@@ -19,6 +20,8 @@ type t = {
   transactions : transaction list
 }
 
+let parent_hash t = t.header.parent_hash
+
 let blockstore = Store.Make (Block.t)
 
 let target_time = 300
@@ -34,8 +37,8 @@ let beneficiary t = t.header.beneficiary
 
 let height t = t.header.height
 
-let next_difficulty (current_block : header): int = 
-  let steps_back = 32 in 
-  let avg_time = (((time_ancestor steps_back current_block) - 
+let next_difficulty (current_block : header): big_int =
+  let steps_back = 32 in
+  let avg_time = (((time_ancestor steps_back current_block) -
                    (current_block.timestamp)) / steps_back) in
-  (target_time / avg_time) * (current_block.difficulty)
+  mult_int_big_int (target_time / avg_time) (current_block.difficulty)
